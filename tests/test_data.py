607118312a7930862e02e17b26b7c72ee291c03a
JTelez <jtadmiss@gmail.com>
2024-01-29 16:00:09 -0500

    Clean up

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,12 +1,19 @@
 import unittest
 import mricloudpy
+import pandas as pd
 
 class TestData(unittest.TestCase):
+
+    def setUp(self):
+        self.DATA_PATH = 'sample_data_covariate'
+        self.obj = mricloudpy.Data(path=self.DATA_PATH, id_type='numeric')
     
-    def test_Data(self):
-        DATA_PATH = 'sample_data_covariate'
-        obj = mricloudpy.Data(path=DATA_PATH, id_type='numeric')  # replace with your actual function
-        self.assertIsInstance(obj, mricloudpy.data.Data)  # replace with your actual class
+    def test_data(self):
+        self.assertIsInstance(self.obj, mricloudpy.data.Data)
+        self.assertEqual(self.obj.path, self.DATA_PATH)
+        self.assertEqual(self.obj.id_type, 'numeric')
+        self.assertEqual(self.obj.id_list, None)
+        self.assertTrue(self.obj.df.equals(self.obj.get_data()))
 
 if __name__ == '__main__':
     unittest.main()
