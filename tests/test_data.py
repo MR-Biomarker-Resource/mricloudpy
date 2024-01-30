38a7683a5cae18b2486fb414d2058283ab6a4fb7
JTelez <jtadmiss@gmail.com>
2024-01-30 13:32:26 -0500

    More tests and cleanup

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,15 +1,16 @@
 import unittest
-import mricloudpy
-import pandas as pd
+from mricloudpy import data
 
 class TestData(unittest.TestCase):
 
     def setUp(self):
+        # Create data object and relevant testing items
         self.DATA_PATH = 'sample_data_covariate'
-        self.obj = mricloudpy.Data(path=self.DATA_PATH, id_type='numeric')
+        self.obj = data.Data(path=self.DATA_PATH, id_type='numeric')
     
     def test_data(self):
-        self.assertIsInstance(self.obj, mricloudpy.data.Data)
+        # Check Data object type and if parameters are valid
+        self.assertIsInstance(self.obj, data.Data)
         self.assertEqual(self.obj.path, self.DATA_PATH)
         self.assertEqual(self.obj.id_type, 'numeric')
         self.assertEqual(self.obj.id_list, None)
